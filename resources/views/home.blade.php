5914458ca70ba92947d498285b1643079bb24b5c
Daniel Wang <danielwang0989@gmail.com>
2025-03-13 14:43:07 -0700

    Header, Footer, and Master Layout created, added a homepage to routes

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.master')
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="post-title">Id and id your plants here:</h1>
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h1 class="post-title">Plants Info Card</h1>
+            <p>Info on plants displayed upon search</p>
+            <p><a href="#">Link to read more?</a></p>
+        </div>
+    </div>
+    <hr>
+@endsection
